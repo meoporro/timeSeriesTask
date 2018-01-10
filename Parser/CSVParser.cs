153b6a80d1a1c9f5e9135cda3be0356c2797230b
Matteo Porro <meoo85@gmail.com>
2018-01-10 18:43:55 +0000

    Project creating and implementation of the parser.

---
+++ Parser/CSVParser.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class CSVParser : IParser
+    {
+        private static CSVParser instance;
+
+        private CSVParser()
+        {
+        }
+
+        public static CSVParser CSVParserInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CSVParser();
+                }
+                return instance;
+            }
+        }
+
+        public double[] ParseDataSeries(string path)
+        {
+            List<double> dataSeries = new List<double>();
+
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    dataSeries.AddRange(line.Split(',').Select(x => double.Parse(x)));
+                }
+            }
+
+            return dataSeries.ToArray();
+        }
+    }
+}
---
+++ timeSeriesTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeSeriesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataPath = "./data/SampleData.csv";
+
+            double dataSeriesMean = 0d;
+            double dataSeriesStandardDeviation = 0d;
+            int numberOfDataPoints = 0;
+
+            
+
+
+        }
+    }
+}

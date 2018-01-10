06ce6886ef30bf95079d2955bad269a49f83887f
Matteo Porro <meoo85@gmail.com>
2018-01-10 20:08:28 +0000

    Mean implemented

---
+++ Engine/CalculatorFactory.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public enum CalculatorType
+    {
+        Null,
+        Mean,
+        StandardDeviation
+    }
+
+    public class CalculatorFactory
+    {
+        public static ICalculator GetCalculator(CalculatorType calculatorType)
+        {
+            switch (calculatorType)
+            {
+                case CalculatorType.Mean:
+                    return new MeanCalculator();
+                case CalculatorType.StandardDeviation:
+                    throw new NotImplementedException();
+                default:
+                    throw new ArgumentException();
+            }
+        }
+    }
+}
---
+++ Engine/MeanCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MeanCalculator : ICalculator
+    {
+        internal MeanCalculator()
+        { }
+
+        public double Compute(IEnumerable<double> values)
+            {
+            double sum = 0;
+            int counter = 0;
+            foreach (double value in values)
+            {
+                sum += value;
+                counter++;
+            }
+
+            return sum / counter;
+        }
+    }
+}

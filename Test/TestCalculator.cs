1dc6d357e885d4f6061cd726b8dcc58d025b8a0d
Matteo Porro <meoo85@gmail.com>
2018-01-10 20:29:04 +0000

    Add test project that was left behind.

---
+++ Test/TestCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void MeanOfConstantArray()
+        {
+            double constant = 1d;
+            int n = 10;
+            double[] constantArray = new double[n].Select(x => constant).ToArray();
+            double arrayMean = workerTestCalculator(CalculatorType.Mean, constantArray);
+            Assert.AreEqual(constant, arrayMean);
+        }
+
+        [TestMethod]
+        public void MeanOfLinearArray()
+        {
+            int n = 10;
+            double[] linearArray = Enumerable.Range(1, n).Select(x => (double)x).ToArray();
+            double arrayMean = workerTestCalculator(CalculatorType.Mean, linearArray);
+            Assert.AreEqual((1 + n) / 2d, arrayMean);
+        }
+
+        [TestMethod]
+        public void MeanOfSquaresArray()
+        {
+            int n = 10;
+            double[] squaresArray = Enumerable.Range(1, n).Select(x => (double)(x * x)).ToArray();
+            double arrayMean = workerTestCalculator(CalculatorType.Mean, squaresArray);
+            Assert.AreEqual((n + 1) * (2 * n + 1) / 6d, arrayMean);
+        }
+
+        private double workerTestCalculator(CalculatorType calculatorType, double[] array)
+        {
+            var calculator = CalculatorFactory.GetCalculator(calculatorType);
+            return calculator.Compute(array);
+        }
+    }
+}
---
+++ Test/TestParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+using System.Linq;
+
+namespace Test
+{
+    [TestClass]
+    public class TestParser
+    {
+        [TestMethod]
+        public void CSVParserSingleLine()
+        {
+            var parser = CSVParser.CSVParserInstance;
+            string dataPath = "../../data/testDataSingleLine.csv";
+            double[] parsedDataSeries = parser.ParseDataSeries(dataPath);
+            CollectionAssert.AreEqual(parsedDataSeries, Enumerable.Range(0, 6).Select(x => (double)x).ToArray());
+        }
+        [TestMethod]
+        public void CSVParserTwoLines()
+        {
+            var parser = CSVParser.CSVParserInstance;
+            string dataPath = "../../data/testDataTwoLines.csv";
+            double[] parsedDataSeries = parser.ParseDataSeries(dataPath);
+            CollectionAssert.AreEqual(parsedDataSeries, Enumerable.Range(0, 11).Select(x => (double)x).ToArray());
+        }
+
+    }
+}
